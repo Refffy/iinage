66009ce7d168f7ec4a595348112b3df461b94fdf
Refffy <53311886+Refffy@users.noreply.github.com>
2021-01-30 06:46:20 +0600

    Add files via upload

---
+++ iin_age/back/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from routes import route
+
+import uvicorn
+
+
+app = FastAPI()
+app.include_router(route)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
---
+++ iin_age/back/routes.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+from fastapi import APIRouter
+
+from datetime import date
+
+import json
+
+
+class Person(BaseModel):
+    iin: str
+
+
+route = APIRouter()
+
+
+@ route.get('/people/{iin}', status_code=200)
+async def get_age(iin: str):
+    with open('person.json', 'r') as f:
+        data = json.load(f)
+
+    if data['iin'] == iin:
+        return {"iin": iin, "age": data['age']}
+
+
+@ route.post('/people/', status_code=201)
+async def post_age(pers: Person):
+    iin_date = iter(pers.iin[0:6])
+    # splitted_iin = [i + next(iin_date) for i in iin_date]
+    splitted_iin = []
+    for i in iin_date:
+        try:
+            splitted_iin.append(i + next(iin_date))
+        except StopIteration:
+            pass
+
+    iin_2_int = list(map(int, splitted_iin))
+    curr_date = list(map(int, date.today().strftime("%y %m %d").split(' ')))
+
+    curr_dt = date(curr_date[0], curr_date[1], curr_date[2])
+    iin_dt = date(iin_2_int[0], iin_2_int[1], iin_2_int[2])
+
+    # age = ((curr_dt - iin_dt) /
+    #        365).days if iin_dt < curr_dt else str(date.today().year - iin_2_int[0])[2:]
+
+    if iin_dt < curr_dt:
+        age = ((curr_dt - iin_dt) / 365).days
+    else:
+        age = str(date.today().year - iin_2_int[0])[2:]
+
+    data = {**pers.dict(), "age": int(age)}
+    with open('person.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    return data
