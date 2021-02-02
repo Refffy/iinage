cd55a2c831af234878c16b56cbe94f2bf5e34407
Refffy <53311886+Refffy@users.noreply.github.com>
2021-02-03 01:40:53 +0600

    Update routes.py

--- iin_age/back/routes.py
+++ iin_age/back/routes.py
@@ -36,19 +36,22 @@ async def post_age(pers: Person):
     iin_2_int = list(map(int, splitted_iin))
     curr_date = list(map(int, date.today().strftime("%y %m %d").split(' ')))
 
-    curr_dt = date(curr_date[0], curr_date[1], curr_date[2])
-    iin_dt = date(iin_2_int[0], iin_2_int[1], iin_2_int[2])
+    try:
+        curr_dt = date(curr_date[0], curr_date[1], curr_date[2])
+        iin_dt = date(iin_2_int[0], iin_2_int[1], iin_2_int[2])
+    except ValueError: pass
 
     # age = ((curr_dt - iin_dt) /
     #        365).days if iin_dt < curr_dt else str(date.today().year - iin_2_int[0])[2:]
 
     if iin_dt < curr_dt:
-        age = ((curr_dt - iin_dt) / 365).days
+        age = ((curr_dt - iin_dt) / 365.25).days
     else:
         age = str(date.today().year - iin_2_int[0])[2:]
 
     data = {**pers.dict(), "age": int(age)}
+
     with open('person.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-    return data
+    return data if len(pers.iin) == 12 else 'Invalid IIN: length > 12!'
